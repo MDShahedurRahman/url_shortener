f860eddc4a2b112d1323710f207ab18f50317f03
Md Shahedur Rahman <mr6139@nyu.edu>
2026-01-25 21:20:17 -0500

    Implement load method

--- storage/json_storage.py
+++ storage/json_storage.py
@@ -9,3 +9,9 @@ class JsonStorage:
     def save(self, data):
         with open(self.FILE_NAME, "w") as f:
             json.dump(data, f, indent=4)
+
+    def load(self):
+        if not os.path.exists(self.FILE_NAME):
+            return []
+        with open(self.FILE_NAME, "r") as f:
+            return json.load(f)
