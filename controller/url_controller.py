da18403aed66c33c74f1166a7effaa9d93ab852c
Md Shahedur Rahman <mr6139@nyu.edu>
2026-01-27 23:38:28 -0500

    Implement menu loop

--- controller/url_controller.py
+++ controller/url_controller.py
@@ -7,3 +7,27 @@ class UrlController:
     def __init__(self):
         self.repo = UrlRepository()
         self.view = UrlView()
+
+    def run(self):
+        while True:
+            choice = self.view.show_menu()
+
+            if choice == "1":
+                self.create_short_url()
+            elif choice == "2":
+                self.resolve_url()
+            elif choice == "3":
+                self.show_all_urls()
+            elif choice == "4":
+                self.delete_url()
+            elif choice == "5":
+                self.export_analytics()
+            elif choice == "6":
+                self.save_data()
+            elif choice == "7":
+                self.load_data()
+            elif choice == "0":
+                print("Goodbye!")
+                break
+            else:
+                print("Invalid option")
